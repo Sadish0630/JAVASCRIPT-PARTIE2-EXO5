f9e380f6a0a93a4e94e5616836afa78807d95004
audemar <audemarsadish@gmail.com>
2021-11-18 16:03:33 +0100

    exo2 terminé

---
+++ exo5.js
@@ -0,0 +1,26 @@
+let day = 4
+switch (day) {
+    case 1:
+      alert("Lundi")
+      break;
+    case 2:
+      alert("Mardi")
+     break;
+     case 3:
+        alert("Mercredi")
+       break;
+       case 4:
+        alert("Jeudi")
+       break;
+       case 5:
+        alert("Vendredi")
+       break;
+       case 6:
+        alert("Samedi")
+       break;
+       case 7:
+        alert("Dimanche")
+       break;
+default:
+        alert("Ce nombre ne correspond à aucun jour")
+       }
